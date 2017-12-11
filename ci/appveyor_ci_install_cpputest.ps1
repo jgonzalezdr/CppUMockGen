5e9bef71657abfde8c5f28bc0e1f9cf5e3440dc9
Jesús González <jgonzalez@gdr-sistemas.com>
2017-12-11 03:24:35 +0100

    Fix install script

--- ci/appveyor_ci_install_cpputest.ps1
+++ ci/appveyor_ci_install_cpputest.ps1
@@ -5,4 +5,4 @@ $cpputest_dir  = "C:\cpputest"
 
 Start-FileDownload 'https://github.com/jgonzalezdr/cpputest/releases/download/v3.7.2-custom/cpputest-3.7.2-custom.zip'
 New-Item -ItemType Directory -Force -Path "$cpputest_dir" | Out-Null
-Invoke-Command '7z x -o$cpputest_dir cpputest-3.7.2-custom.zip'
+Invoke-Command "7z x -o$cpputest_dir cpputest-3.7.2-custom.zip"
