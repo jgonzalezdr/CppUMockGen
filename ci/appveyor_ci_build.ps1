24f68b26d923051b4176e090879951b93b0fce08
Jesús González <jgonzalez@gdr-sistemas.com>
2017-12-11 03:03:54 +0100

    Added Appveyor scripts

---
+++ ci/appveyor_ci_build.ps1
@@ -0,0 +1,83 @@
+﻿
+# Load functions from the helper file
+. (Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'appveyor_helpers.ps1')
+
+function Invoke-CygwinCommand($command, $directory = '.')
+{
+    # Assume cygwin is located at C:\cygwin on x86 and C:\cygwin64 on x64 for now
+    $cygwin_bin = Get-CygwinBin
+
+    $cygwin_directory = (. "${cygwin_bin}\cygpath.exe" (Resolve-Path $directory))
+    $command_wrapped = "${cygwin_bin}\bash.exe --login -c 'cd $cygwin_directory ; $command'"
+    
+    Write-Host "Executing <$command> in <$cygwin_directory>"
+    Invoke-Expression $command_wrapped
+
+    if ($LASTEXITCODE -ne 0)
+    {
+        Write-Host "Command Returned error: $LASTEXITCODE"
+        Exit $LASTEXITCODE
+    }
+}
+
+# Clean up some paths for any configuration
+#Remove-PathFolder "C:\MinGW\bin"
+#Remove-PathFolder "C:\Program Files\Git\bin"
+#Remove-PathFolder "C:\Program Files\Git\cmd"
+#Remove-PathFolder "C:\Program Files\Git\usr\bin"
+#Remove-PathFolder "C:\Program Files (x86)\Git\bin"
+#Remove-PathFolder "C:\Program Files (x86)\Git\cmd"
+#Remove-PathFolder "C:\Program Files (x86)\Git\usr\bin"
+
+$env:CPPUTEST_HOME='C:\cpputest\'
+
+$build_dir = "build"
+
+New-Item -ItemType Directory -Force -Path "$build_dir" | Out-Null
+
+switch -Wildcard ($env:Platform)
+{
+    'Cygwin*'
+    {
+        Invoke-CygwinCommand "autoreconf -i .." "$build_dir"
+        Invoke-CygwinCommand "../configure" "$build_dir"
+        Invoke-CygwinCommand "make CppUTestTests.exe CppUTestExtTests.exe" "$build_dir"
+    }
+
+    'MinGW*'
+    {
+        $mingw_path = Get-MinGWBin
+
+        # Add mingw to the path
+        Add-PathFolder $mingw_path
+
+        Invoke-Command "cmake -G 'MinGW Makefiles' .." '$build_dir'
+        Invoke-Command "mingw32-make all" '$build_dir'
+
+        Remove-PathFolder $mingw_path
+    }
+
+    'MSVC*'
+    {
+        if ($env:APPVEYOR)
+        {
+            $logger_arg = '/logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"'
+        }
+        else
+        {
+            $logger_arg = ''
+        }
+
+        if ($env:Platform -like '*32')
+        {
+            $cmake_generator = "Visual Studio 15 Win32"
+        }
+        else
+        {
+            $cmake_generator = "Visual Studio 15 Win64"
+        }
+
+        Invoke-Command "cmake .. -G '$cmake_generator' -Wno-dev -DCI_MODE=ON" "$build_dir"
+        Invoke-Command "msbuild /ToolsVersion:15.0 $logger_arg build.vcxproj" "$build_dir"
+    }
+}
---
+++ ci/appveyor_ci_install_cpputest.ps1
@@ -0,0 +1,8 @@
+# Load functions from the helper file
+. (Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'appveyor_helpers.ps1')
+
+$cpputest_dir  = "C:\cpputest"
+
+Start-FileDownload 'https://github.com/jgonzalezdr/cpputest/releases/download/v3.7.2-custom/cpputest-3.7.2-custom.zip'
+New-Item -ItemType Directory -Force -Path "$cpputest_dir"
+Invoke-Command '7z -x -o $cpputest_dir cpputest-3.7.2-custom.zip'
