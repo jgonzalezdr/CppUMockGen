4dc25a3440888295d420aea03eee7e4c8b7d48b1
Jesús González <jgonzalez@gdr-sistemas.com>
2018-01-27 04:40:07 +0100

    Fix of coverage build. Maybe. #5

--- ci/appveyor_ci_install_lcov.ps1
+++ ci/appveyor_ci_install_lcov.ps1
@@ -9,4 +9,5 @@ if ($env:Configuration -eq 'Coverage')
 {
     Start-FileDownload 'https://github.com/jgonzalezdr/lcov/releases/download/v1.11w.1/lcov-v1.11w.1.zip'
     Invoke-Command "7z x -oC:\ lcov-v1.11w.1.zip" | Out-Null
+    Invoke-Command "cpanm --quiet --installdeps --notest C:\lcov\bin"
 }
