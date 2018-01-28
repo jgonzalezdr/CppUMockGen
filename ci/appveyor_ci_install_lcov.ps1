207e22337e9eea255a53e4a11a4dbc357f0f58f8
Jesús González <jgonzalez@gdr-sistemas.com>
2018-01-28 20:02:40 +0100

    Fixed LCOV download URL

--- ci/appveyor_ci_install_lcov.ps1
+++ ci/appveyor_ci_install_lcov.ps1
@@ -5,5 +5,5 @@
 # Load functions from the helper file
 . (Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'appveyor_helpers.ps1')
 
-Start-FileDownload 'https://github.com/jgonzalezdr/lcov/releases/download/v1.11w.1/lcov-v1.11w.1.zip'
-Invoke-Command "7z x -oC:\ lcov-v1.11w.1.zip" | Out-Null
+Start-FileDownload 'https://github.com/jgonzalezdr/lcov/releases/download/v1.11.1w/lcov-v1.11.1w.zip'
+Invoke-Command "7z x -oC:\ lcov-v1.11.1w.zip" | Out-Null
