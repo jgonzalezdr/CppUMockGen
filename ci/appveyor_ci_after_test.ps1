341d2aa2c2fdd504092dd6d181ebbd0e46ec1dce
Jesús González <jgonzalez@gdr-sistemas.com>
2018-06-24 23:59:26 +0200

    Added installer generation to CI

--- ci/appveyor_ci_after_test.ps1
+++ ci/appveyor_ci_after_test.ps1
@@ -2,6 +2,9 @@
 # Script to after tests phase in AppVeyor
 #
 
+# Load functions from the helper file
+. (Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'appveyor_helpers.ps1')
+
 $build_dir = "build"
 
 if (-not $env:APPVEYOR)
@@ -12,7 +15,37 @@ if (-not $env:APPVEYOR)
     }
 }
 
+if ($env:GenerateInstaller)
+{
+    $build_dir = "build"
+    
+    if(!($env:Configuration -eq 'Release'))
+    {
+        throw "Installer generation is only supported for release builds"
+    }
+    
+    switch -Wildcard ($env:Platform)
+    {
+        'MinGW*'
+        {
+            $mingw_path = Get-MinGWBin
+
+            # Add mingw to the path
+            Add-PathFolder $mingw_path
+
+            Invoke-Command "mingw32-make package" "$build_dir"
+
+            Remove-PathFolder $mingw_path
+        }
+
+        default
+        {
+            throw "Installer generation is only supported for MinGW builds"
+        }
+    }
+}
+
 if ($env:PublishArtifacts)
 {
-    Get-ChildItem $build_dir\app\*.exe | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+    Get-ChildItem $build_dir\*.exe | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 }
