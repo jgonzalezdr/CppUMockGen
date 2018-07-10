789d764dab5f8a7b41dbb3683558c646067c02c6
Jesús González <jgonzalez@gdr-sistemas.com>
2018-07-10 03:17:31 +0200

    Update VSHelper to accept more file types

--- cmake/VSHelper.cmake
+++ cmake/VSHelper.cmake
@@ -6,8 +6,8 @@
 
 function( generate_groups BASE_DIR CURR_DIR )
     string( REPLACE "/" "\\" GROUP_NAME "${CURR_DIR}" )
-    source_group( "Source Files\\${GROUP_NAME}" REGULAR_EXPRESSION ${BASE_DIR}/${CURR_DIR}.*\\.\(cpp\|c\) )
-    source_group( "Header Files\\${GROUP_NAME}" REGULAR_EXPRESSION ${BASE_DIR}/${CURR_DIR}.*\\.\(hpp\|h\) )
+    source_group( "Source Files\\${GROUP_NAME}" REGULAR_EXPRESSION ${BASE_DIR}/${CURR_DIR}.*\\.\(cpp\|c\|cxx\|cc\) )
+    source_group( "Header Files\\${GROUP_NAME}" REGULAR_EXPRESSION ${BASE_DIR}/${CURR_DIR}.*\\.\(hpp\|h\|hxx\|hh\) )
     file( GLOB FILE_LIST RELATIVE ${BASE_DIR} ${BASE_DIR}/${CURR_DIR}/* )
     foreach( FILE ${FILE_LIST} )
         if( IS_DIRECTORY ${BASE_DIR}/${FILE} )
