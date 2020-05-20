b9ef4a6a1bef20d88bebc028d2d76a1cba4af8b4
Jesús González <jgonzalez@gdr-sistemas.com>
2020-05-20 17:31:43 +0200

    Updated Appveyor scripts according to last cmake module changes

--- ci/appveyor_ci_install_cpputest_linux.ps1
+++ ci/appveyor_ci_install_cpputest_linux.ps1
@@ -6,5 +6,5 @@
 . (Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'appveyor_helpers.ps1')
 
 Start-FileDownload 'https://github.com/jgonzalezdr/cpputest/releases/download/v3.9.alpha0/cpputest-3.9.alpha0.txz'
-New-Item -ItemType Directory -Force -Path "$env:CppUTest_HOME" | Out-Null
-Invoke-Command "tar -Jxvf cpputest-3.9.alpha0.txz -C $env:CppUTest_HOME" | Out-Null
+New-Item -ItemType Directory -Force -Path "$env:CPPUTEST_HOME" | Out-Null
+Invoke-Command "tar -Jxvf cpputest-3.9.alpha0.txz -C $env:CPPUTEST_HOME" | Out-Null
