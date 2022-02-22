9d9313f8979bc7ec4cc30a38d948128d758cbb86
Jesús González <jgonzalez@gdr-sistemas.com>
2022-02-22 20:30:43 +0100

    Update continuous integration scripts

--- ci/appveyor_ci_install_cpputest_win.ps1
+++ ci/appveyor_ci_install_cpputest_win.ps1
@@ -5,6 +5,6 @@
 # Load functions from the helper file
 . (Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'appveyor_helpers.ps1')
 
-Start-FileDownload 'https://github.com/jgonzalezdr/cpputest/releases/download/v3.9.alpha0/cpputest-3.9.alpha0.zip'
+Start-FileDownload 'https://github.com/jgonzalezdr/cpputest/releases/download/v4.0-gdr0/cpputest-4.0-gdr0.zip'
 New-Item -ItemType Directory -Force -Path "$env:CPPUTEST_HOME" | Out-Null
-Invoke-Command "7z x -o$env:CPPUTEST_HOME cpputest-3.9.alpha0.zip" | Out-Null
+Invoke-Command "7z x -o$env:CPPUTEST_HOME cpputest-4.0-gdr0.zip" | Out-Null
